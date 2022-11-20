fa26024fbdeaa51b1d3c5c8a72cacf815a767875
mproticc <mprotic11022rn@raf.rs>
2022-11-20 20:29:49 +0100

    first commit

---
+++ app/assets/js/script.js
@@ -0,0 +1,83 @@
+document.getElementById("addCategory").addEventListener("click", function () {
+    const categoryName = document.getElementById("categoryName").value;
+
+    fetch("http://localhost:3000/categories", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            categoryName: categoryName,
+        })
+    }).then(function () {
+        alert("Category added!");
+        renderCategory();
+    })
+});
+
+function renderCategory() {
+    fetch("http://localhost:3000/categories", {
+        method: "GET"
+    })
+        .then(function (data) {
+            return data.json();
+        })
+        .then(function (categories) {
+            console.log(categories)
+
+            // reset if something was in table
+            document.getElementById("tableBody").innerHTML = "";
+
+            for (let category of categories) {
+                const tableRow = document.createElement("tr");
+
+                const tdId = document.createElement("td");
+                tdId.textContent = category.id;
+
+                const tdcategoryName = document.createElement("td");
+                tdcategoryName.textContent = category.categoryName;
+
+
+                const tdDelete = document.createElement("td");
+                const tdDeleteLink = document.createElement("a");
+                tdDeleteLink.textContent = "Delete";
+                tdDeleteLink.href = "#s";
+                tdDeleteLink.classList.add("delete");
+                tdDeleteLink.setAttribute("data-categoryid", category.id);
+
+                tdDelete.appendChild(tdDeleteLink);
+
+                tableRow.appendChild(tdId);
+                tableRow.appendChild(tdcategoryName);
+                tableRow.appendChild(tdDelete);
+
+                document.getElementById("tableBody").appendChild(tableRow);
+            }
+
+            // add click event - DELETE
+            const deleteLinks = document.getElementsByClassName("delete");
+            console.log(deleteLinks);
+
+            for (let deleteLink of deleteLinks) {
+                deleteLink.addEventListener("click", function (event) {
+                    event.preventDefault();
+
+                    console.log(event);
+
+                    const id = event.target.getAttribute("data-categoryid");
+
+                    fetch("http://localhost:3000/categories" + id, {
+                        method: "DELETE"
+                    })
+                        .then(function () {
+                            alert("Category deleted!!");
+                            renderCategory();
+
+                        })
+                })
+            }
+        })
+}
+window.onload = function () {
+    renderCategory();
+}
