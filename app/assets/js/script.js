35b6330707e0973d0bd4622b3d08b7aa28b4705b
mproticc <mprotic11022rn@raf.rs>
2022-11-22 16:11:38 +0100

    ispis kategorija izdvojeno u posebnu funkciju

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -23,57 +23,61 @@ function renderCategory() {
             return data.json();
         })
         .then(function (categories) {
-            console.log(categories)
-            document.getElementById("tableBody").innerHTML = "";
-
-            for (let category of categories) {
-                const tableRow = document.createElement("tr");
-
-                const tdId = document.createElement("td");
-                tdId.textContent = category.id;
-
-                const tdcategoryName = document.createElement("td");
-                tdcategoryName.textContent = category.categoryName;
-
-
-                const tdDelete = document.createElement("td");
-                const tdDeleteLink = document.createElement("a");
-                tdDeleteLink.textContent = "Delete";
-                tdDeleteLink.href = "#s";
-                tdDeleteLink.classList.add("delete");
-                tdDeleteLink.setAttribute("data-categoryid", category.id);
-
-                tdDelete.appendChild(tdDeleteLink);
-
-                tableRow.appendChild(tdId);
-                tableRow.appendChild(tdcategoryName);
-                tableRow.appendChild(tdDelete);
-
-                document.getElementById("tableBody").appendChild(tableRow);
-            }
-            const deleteLinks = document.getElementsByClassName("delete");
-            console.log(deleteLinks);
-
-            for (let deleteLink of deleteLinks) {
-                deleteLink.addEventListener("click", function (event) {
-                    event.preventDefault();
-
-                    console.log(event);
-
-                    const id = event.target.getAttribute("data-categoryid");
-
-                    fetch(apiHost + id, {
-                        method: "DELETE"
-                    })
-                        .then(function () {
-                            alert("Category deleted!!");
-                            renderCategory();
-
-                        })
-                })
-            }
+            createTable(categories);
         })
 }
 window.onload = function () {
     renderCategory();
+}
+
+function createTable(categories) {
+    console.log(categories)
+    document.getElementById("tableBody").innerHTML = "";
+
+    for (let category of categories) {
+        const tableRow = document.createElement("tr");
+
+        const tdId = document.createElement("td");
+        tdId.textContent = category.id;
+
+        const tdcategoryName = document.createElement("td");
+        tdcategoryName.textContent = category.categoryName;
+
+
+        const tdDelete = document.createElement("td");
+        const tdDeleteLink = document.createElement("a");
+        tdDeleteLink.textContent = "Delete";
+        tdDeleteLink.href = "#s";
+        tdDeleteLink.classList.add("delete");
+        tdDeleteLink.setAttribute("data-categoryid", category.id);
+
+        tdDelete.appendChild(tdDeleteLink);
+
+        tableRow.appendChild(tdId);
+        tableRow.appendChild(tdcategoryName);
+        tableRow.appendChild(tdDelete);
+
+        document.getElementById("tableBody").appendChild(tableRow);
+    }
+    const deleteLinks = document.getElementsByClassName("delete");
+    console.log(deleteLinks);
+
+    for (let deleteLink of deleteLinks) {
+        deleteLink.addEventListener("click", function (event) {
+            event.preventDefault();
+
+            console.log(event);
+
+            const id = event.target.getAttribute("data-categoryid");
+
+            fetch(apiHost + id, {
+                method: "DELETE"
+            })
+                .then(function () {
+                    alert("Category deleted!!");
+                    renderCategory();
+
+                })
+        })
+    }
 }
